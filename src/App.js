d9c54dfd644f2e69f85d121c18b2f5aa764d35ef
LucianaChamma <lusahd@hotmail.com>
2023-07-04 21:30:47 -0300

    agora vai

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+
+
+export default () => {
+
+ const[movieList, setMovieList]= useState ([]);
+ 
+  useEffect(()=>{ //carregar o conteúdo
+    const loadAll = async () => {
+      //Pegando a lista TOTAL
+     let list = await Tmdb.getHomeList(); //Await =>espera a requisição da api para carregar o conteúdo
+     setMovieList(list);
+    }
+   
+    loadAll();
+  }, []);
+
+  return (
+    <div className = "page">
+     <section className="lists">
+       {movieList.map((item, key)=>(
+         <MovieRow key={key} title={item.title} items={item.items} />
+       ))}
+     </section>
+    </div>
+ );
+ }
+
